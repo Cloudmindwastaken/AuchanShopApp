b9581268e45a6a09007ca866cff1703e0711c60b
Cloudmindwastaken <45768779+Cloudmindwastaken@users.noreply.github.com>
2020-06-10 10:00:52 +0200

    Add files via upload

---
+++ PROJEKT PROGRAMOWANIE/Views/Window2.xaml.cs
@@ -0,0 +1,78 @@
+﻿using LKuznecki.Models;
+using LKuznecki.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LKuznecki.Views
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+
+
+        }
+        public CrudViewModel CrudViewModel { get; }
+        public Window2(CrudViewModel crudViewModel)
+        {
+            CrudViewModel = crudViewModel;
+            DataContext = crudViewModel;
+            InitializeComponent();
+
+        }
+
+
+
+        private void GotowkaButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window4 objWindow4 = new Window4();
+            this.Visibility = Visibility.Hidden; // Chowamy akutalne okno
+            objWindow4.Show();
+        }
+
+        private void PayUButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window4 objWindow4 = new Window4();
+            this.Visibility = Visibility.Hidden; // Chowamy akutalne okno
+            objWindow4.Show();
+        }
+
+        private void BonButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window4 objWindow4 = new Window4();
+            this.Visibility = Visibility.Hidden; // Chowamy akutalne okno
+            objWindow4.Show();
+        }
+
+        private void resultButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            double suma = 0;
+            foreach (var item in CrudViewModel.RightItemsList)
+            {
+                suma += item.ItemPrice;
+            }
+            resultLabel.Content = suma.ToString();
+        }
+        
+        private void PlatnoscKartaButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window3 objWindow3 = new Window3();
+            this.Visibility = Visibility.Hidden; // Chowamy akutalne okno
+            objWindow3.Show();
+        }
+    }
+}
---
+++ PROJEKT PROGRAMOWANIE/Views/Window3.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using LKuznecki.Views;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Linq;
+
+namespace LKuznecki.Views
+{
+    /// <summary>
+    /// Interaction logic for Window3.xaml
+    /// Deklaracja funkcjonalności przycisków interfejsu do płatności kartą
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "8";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}8";
+            }
+        }
+        private void eightButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "8";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}8";
+            }
+        }
+
+        private void nineButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "9";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}9";
+            }
+        }
+
+        private void fourButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "4";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}4";
+            }
+        }
+
+        private void fiveButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "5";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}5";
+            }
+        }
+
+        private void sixButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "6";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}6";
+            }
+        }
+
+        private void oneButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "1";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}1";
+            }
+        }
+
+        private void twoButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "2";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}2";
+            }
+        }
+
+        private void threeButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "3";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}3";
+            }
+        }
+
+        private void zeroButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Count() >= 4)
+                return;
+            try
+            {
+                if (resultLabel.Content.ToString() == "0")
+                {
+                    resultLabel.Content = "0";
+                }
+                else
+                {
+                    resultLabel.Content = $"{resultLabel.Content}0";
+                }
+            }
+            catch (Exception ex)
+            {
+                resultLabel.Content = ex.Message;
+            }
+        }
+
+        private void EnterButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window4 objWindow4 = new Window4();
+            this.Visibility = Visibility.Hidden; // Chowamy akutalne okno
+            objWindow4.Show();
+
+        }
+
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+        // W przypadku złego wpisania możemy wyczyścić pin i wpisać go od nowa
+    }
+}
